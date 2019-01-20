5a0aa3e0b9423ff31b6da1614e50f6c658f1d232
chenxue <melthaw@gmail.com>
2019-01-20 20:05:20 +0800

    fix data mongodb

---
+++ module/gridfs/src/main/java/in/clouthink/daas/fss/mongodb/impl/DefaultStoredFileObject.java
@@ -0,0 +1,69 @@
+package in.clouthink.daas.fss.mongodb.impl;
+
+import com.mongodb.gridfs.GridFSDBFile;
+import in.clouthink.daas.fss.core.StoreFileRequest;
+import in.clouthink.daas.fss.core.StoredFileObject;
+import in.clouthink.daas.fss.domain.model.FileObject;
+import in.clouthink.daas.fss.support.DefaultFileObject;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.BeanUtils;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class DefaultStoredFileObject extends DefaultFileObject implements StoredFileObject {
+
+    public static DefaultStoredFileObject from(StoreFileRequest request) {
+        if (request == null) {
+            return null;
+        }
+        DefaultStoredFileObject result = new DefaultStoredFileObject();
+        BeanUtils.copyProperties(request, result);
+        return result;
+    }
+
+    public static DefaultStoredFileObject from(FileObject fileObject) {
+        if (fileObject == null) {
+            return null;
+        }
+        DefaultStoredFileObject result = new DefaultStoredFileObject();
+        BeanUtils.copyProperties(fileObject, result);
+        return result;
+    }
+
+    private static final Log logger = LogFactory.getLog(DefaultStoredFileObject.class);
+
+    private String providerName;
+
+    private GridFSDBFile gridFSDBFile;
+
+    @Override
+    public String getProviderName() {
+        return providerName;
+    }
+
+    public void setProviderName(String providerName) {
+        this.providerName = providerName;
+    }
+
+    @Override
+    public GridFSDBFile getImplementation() {
+        return gridFSDBFile;
+    }
+
+    public void setImplementation(GridFSDBFile gridFSDBFile) {
+        this.gridFSDBFile = gridFSDBFile;
+    }
+
+    @Override
+    public void writeTo(OutputStream outputStream, long bufferSize) throws IOException {
+        if (getImplementation() == null) {
+            throw new UnsupportedOperationException("The stored file implementation is null.");
+        }
+        logger.warn(String.format("The bufferSize is not supported in current provider, the value[%d] will be ignored.",
+                                  bufferSize));
+        getImplementation().writeTo(outputStream);
+    }
+
+}
